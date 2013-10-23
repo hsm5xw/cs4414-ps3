4331d3dc636f645c5528a5fd3218444427d340b3
UVa student <student@student-VirtualBox.(none)>
2013-10-22 22:48:47 -0400

    updated starting code from Weilin tp version 0.2

--- zhtta.rs
+++ zhtta.rs
@@ -4,26 +4,26 @@
 // Running on Rust 0.8
 //
 // Starting code for PS3
-// 
+//
 // Note: it would be very unwise to run this server on a machine that is
 // on the Internet and contains any sensitive files!
 //
 // University of Virginia - cs4414 Fall 2013
 // Weilin Xu and David Evans
-// Version 0.1
+// Version 0.2
 
 extern mod extra;
 
 use std::rt::io::*;
-use std::rt::io::net::ip::{SocketAddr, Ipv4Addr};
+use std::rt::io::net::ip::SocketAddr;
 use std::io::println;
 use std::cell::Cell;
 use std::{os, str, io};
 use extra::arc;
 use std::comm::*;
 
-static PORT:    int = 4414;
-static IPV4_LOOPBACK: &'static str = "127.0.0.1";
+static PORT: int = 4414;
+static IP: &'static str = "0.0.0.0";
 static mut visitor_count: uint = 0;
 
 struct sched_msg {
@@ -42,34 +42,53 @@ fn main() {
     
     // add file requests into queue.
     do spawn {
-        while(true) {
+        loop {
             do add_vec.write |vec| {
-                let tf:sched_msg = port.recv();
-                (*vec).push(tf);
-                println("add to queue");
+                // port.recv() will block the code and keep locking the RWArc, so we simply use peek() to check if there's message to recv.
+                // But a asynchronous solution will be much better.
+                if (port.peek()) {
+                    let tf:sched_msg = port.recv();
+                    (*vec).push(tf);
+                    println(fmt!("add to queue, size: %ud", (*vec).len()));
+                }
             }
         }
     }
     
     // take file requests from queue, and send a response.
+    // FIFO
     do spawn {
-        while(true) {
+        loop {
             do take_vec.write |vec| {
-                let mut tf = (*vec).pop();
-                
-                match io::read_whole_file(tf.filepath) {
-                    Ok(file_data) => {
-                        tf.stream.write(file_data);
-                    }
-                    Err(err) => {
-                        println(err);
+                if ((*vec).len() > 0) {
+                    // FILO didn't make sense in service scheduling, so we modify it as FIFO by using shift_opt() rather than pop().
+                    let tf_opt: Option<sched_msg> = (*vec).shift_opt();
+                    let mut tf = tf_opt.unwrap();
+                    println(fmt!("shift from queue, size: %ud", (*vec).len()));
+
+                    match io::read_whole_file(tf.filepath) { // killed if file size is larger than memory size.
+                        Ok(file_data) => {
+                            println(fmt!("begin serving file [%?]", tf.filepath));
+                            // A web server should always reply a HTTP header for any legal HTTP request.
+                            tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
+                            tf.stream.write(file_data);
+                            println(fmt!("finish file [%?]", tf.filepath));
+                        }
+                        Err(err) => {
+                            println(err);
+                        }
                     }
                 }
             }
         }
     }
     
-    let socket = net::tcp::TcpListener::bind(SocketAddr {ip: Ipv4Addr(127,0,0,1), port: PORT as u16});
+    let ip = match FromStr::from_str(IP) { Some(ip) => ip,
+                                           None => { println(fmt!("Error: Invalid IP address <%s>", IP));
+                                                     return;},
+                                         };
+                                         
+    let socket = net::tcp::TcpListener::bind(SocketAddr {ip: ip, port: PORT as u16});
     
     println(fmt!("Listening on tcp port %d ...", PORT));
     let mut acceptor = socket.listen().unwrap();
@@ -101,15 +120,18 @@ fn main() {
                     println(fmt!("Request received:\n%s", request_str));
                     let response: ~str = fmt!(
                         "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
-                         <doctype !html><html><head><title>Hello, Rust!</title>
-                         <style>body { background-color: #111; color: #FFEEAA }
-                                h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
-                                h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
-                         </style></head>
-                         <body>
-                         <h1>Greetings, Krusty!</h1>
-                         <h2>Visitor count: %u</h2>
-                         </body></html>\r\n", unsafe{visitor_count});
+			<doctype !html><html>
+			<head><title>Hello, Rust!</title>
+			<style>
+				body { background-color: #111; color: #FFEEAA }
+				h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
+				h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
+			</style>
+			</head>
+			<body>
+			<h1>Greetings, Krusty!</h1>
+			<h2>Visitor count: %u</h2>
+			</body></html>\r\n", unsafe{visitor_count});
 
                     stream.write(response.as_bytes());
                 }
@@ -118,6 +140,7 @@ fn main() {
                     let msg: sched_msg = sched_msg{stream: stream, filepath: file_path.clone()};
                     child_chan.send(msg);
                     
+                    
                     println(fmt!("get file request: %?", file_path));
                 }
             }
@@ -125,3 +148,5 @@ fn main() {
         }
     }
 }
+
+
