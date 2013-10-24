a64adb45019023d075db442a645a3a7f08734ef1
UVa student <student@student-VirtualBox.(none)>
2013-10-24 08:43:42 -0400

    Merged changes from starting code v0.3 into zhtta_jamming. Minor bug fixes from the starting code. Incorporated changed

--- zhtta_jamming.rs
+++ zhtta_jamming.rs
@@ -10,7 +10,7 @@
 //
 // University of Virginia - cs4414 Fall 2013
 // Weilin Xu and David Evans
-// Version 0.2
+// Version 0.3
 
 extern mod extra;
 
@@ -25,7 +25,7 @@ use std::comm::*;
 use std::rt::io::net::tcp::TcpStream;
 
 static PORT: int = 4414;
-static IP: &'static str = "0.0.0.0"; 
+static IP: &'static str = "127.0.0.1"; 
 
 struct sched_msg {
     stream: Option<std::rt::io::net::tcp::TcpStream>,
@@ -92,64 +92,59 @@ fn main() {
     let chan = SharedChan::new(chan);
     
 
-    /* IMPORTANT NOTE: 	Weilin's new code for this adding file requests ('do spawn' part) is buggy. 
-			So don't change the code right below ! 
-    */
-
-    // add file requests into queue.
-    do spawn {
-        while(true) {
-            do add_vec.write |vec| {
-                let tf:sched_msg = port.recv();
-                (*vec).push(tf);
-                println("add to queue");
-            }
-        }
-    }
-    
-    // take file requests from queue, and send a response.
+    // dequeue file requests, and send responses.
     // FIFO
     do spawn {
-        loop {
-            do take_vec.write |vec| {
-                if ((*vec).len() > 0) {
-                    // FILO didn't make sense in service scheduling, so we modify it as FIFO by using shift_opt() rather than pop().
-                    let tf_opt: Option<sched_msg> = (*vec).shift_opt();
-                    let mut tf = tf_opt.unwrap();
-                    println(fmt!("shift from queue, size: %ud", (*vec).len()));
-
-                    match io::read_whole_file(tf.filepath) { // killed if file size is larger than memory size.
-                        Ok(file_data) => {
-                            println(fmt!("begin serving file [%?]", tf.filepath));
-                            // A web server should always reply a HTTP header for any legal HTTP request.
-                            tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream; charset=UTF-8\r\n\r\n".as_bytes());
-                            tf.stream.write(file_data);
-                            println(fmt!("finish file [%?]", tf.filepath));
-                        }
-                        Err(err) => {
-                            println(err);
-                        }
+        let (sm_port, sm_chan) = stream();
+        
+        // a task for sending responses.
+        do spawn {
+            loop {
+                let mut tf: sched_msg = sm_port.recv(); // wait for the dequeued request to handle
+                match io::read_whole_file(tf.filepath) { // killed if file size is larger than memory size.
+                    Ok(file_data) => {
+                        println(fmt!("begin serving file [%?]", tf.filepath));
+                        // A web server should always reply a HTTP header for any legal HTTP request.
+                        tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n".as_bytes());
+                        tf.stream.write(file_data);
+                        println(fmt!("finish file [%?]", tf.filepath));
+                    }
+                    Err(err) => {
+                        println(err);
                     }
                 }
             }
         }
+        
+        loop {
+            port.recv(); // wait for arrving notification
+            do take_vec.write |vec| {
+                if ((*vec).len() > 0) {
+                    // LIFO didn't make sense in service scheduling, so we modify it as FIFO by using shift_opt() rather than pop().
+                    let tf_opt: Option<sched_msg> = (*vec).shift_opt();
+                    let tf = tf_opt.unwrap();
+                    println(fmt!("shift from queue, size: %ud", (*vec).len()));
+                    sm_chan.send(tf); // send the request to send-response-task to serve.
+                }
+            }
+        }
     }
+
 
     let ip = match FromStr::from_str(IP) { Some(ip) => ip,
 					   None     => {println(fmt!("Error: Invalid IP address <%s>", IP));
 						        return;}
  					 };
     
-    let socket = net::tcp::TcpListener::bind(SocketAddr {ip: ip, port: PORT as u16}); 
 
-    println(fmt!("Listening on tcp port %d ...", PORT));
+    let socket = net::tcp::TcpListener::bind(SocketAddr {ip: ip, port: PORT as u16});
+    
+    println(fmt!("Listening on %s:%d ...", ip.to_str(), PORT));
     let mut acceptor = socket.listen().unwrap();
+
 
     let shared_count = arc::RWArc::new( visitor_count ); // @@@@@@  
 
-
-    // we can limit the incoming connection count.
-    //for stream in acceptor.incoming().take(10 as uint) {
 
     for stream in acceptor.incoming() {
 	let mut stream = stream; // @@@@@@ 
@@ -170,7 +165,6 @@ fn main() {
 	} 			// @@@@@
 
 	isWahoo = is_Wahoo_Client(peer_addr); 
-
 	println( fmt!("is wahoo? : %?\n", isWahoo) );
 
 
@@ -182,6 +176,7 @@ fn main() {
 
         // Start a new task to handle the connection
         let child_chan = chan.clone();
+	let child_add_vec = add_vec.clone();
 
         do spawn {
        
@@ -194,8 +189,6 @@ fn main() {
     	    }
 	   	        	        	    
             let mut stream = stream.take();
-
-
             let mut buf = [0, ..500];
             stream.read(buf);
             let request_str = str::from_utf8(buf);
@@ -233,12 +226,20 @@ fn main() {
 
                 }
                 else {
-                    // may do scheduling here
-	    
-		    let size:int = get_fileSize(file_path);
+                    // Requests scheduling
+
+		    let size:int = get_fileSize(file_path); // @@@@@@@@@@@
 
                     let msg: sched_msg = sched_msg{stream: stream, filepath: file_path.clone()};
-                    child_chan.send(msg);
+                    let (sm_port, sm_chan) = std::comm::stream();
+                    sm_chan.send(msg);
+                    
+                    do child_add_vec.write |vec| {
+                        let msg = sm_port.recv();
+                        (*vec).push(msg); // enqueue new request.
+                        println("add to queue");
+                    }
+                    child_chan.send(""); //notify the new arriving request.
                     
                     println(fmt!("get file request: %?", file_path));
                 }
