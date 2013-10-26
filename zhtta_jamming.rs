054fde6e12998806992b0f8d01a7f133570d1c67
UVa student <student@student-VirtualBox.(none)>
2013-10-26 11:51:08 -0400

    added server side gashing

--- zhtta_jamming.rs
+++ zhtta_jamming.rs
@@ -28,6 +28,9 @@ use std::rt::io::net::tcp::TcpStream;
 use std::cmp::Ord;
 use std::num::log2;
 
+use std::{run, path, libc};
+use std::task;
+
 use extra::priority_queue::*;
 
 static PORT: int = 4414;
@@ -146,7 +149,8 @@ fn main() {
                         		// A web server should always reply a HTTP header for any legal HTTP request.
                         		tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n".as_bytes());
                         		let data = cache.get_copy(&tf.filepath.to_str());
-                        		tf.stream.write(data);
+					let new_file_data = check_SSI(tf.filepath.to_str(),data.clone());  //#
+                        		tf.stream.write(new_file_data);
 					fileAccessTimes.insert(tf.filepath.to_str(), ~time::get_time().sec);
                         		println(fmt!("finish file [%?]", tf.filepath));
                 		}
@@ -160,7 +164,8 @@ fn main() {
                           				println(fmt!("begin serving file [%?]", tf.filepath));
                           				// A web server should always reply a HTTP header for any legal HTTP request.
                           				tf.stream.write("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n".as_bytes());
-                                			tf.stream.write(file_data);
+							let new_file_data = check_SSI(tf.filepath.to_str(),file_data.clone()); //#
+                                			tf.stream.write(new_file_data);
                                 			println(fmt!("finish file [%?]", tf.filepath));
                                 			let file_size = get_fileSize(tf.filepath);
 							if file_size < maxCacheFileSize {
@@ -352,5 +357,158 @@ fn main() {
     }
 }
 
+fn check_SSI(pathstr: &str, file_data:  ~[u8]) -> ~[u8] {
+	//let pathstr = file_path.to_str();
+	if pathstr.contains(".shtml") {
+		let mut data = std::str::from_utf8_owned(file_data);
+		if data.contains("<!--#exec cmd=\"") {
+			let startbyte = data.find_str("<!--#exec cmd=\"").unwrap();
+			let endbyte = data.find_str("\" -->").unwrap();
+			let command = (data.slice(startbyte+15, endbyte).to_owned());
+			println(command);
+			println("hello");
+			let frontdata = data.slice_to(startbyte).to_owned();
+			let backdata = data.slice_from(endbyte+5).to_owned();
+			let result = "<p>" + gash_handle(command) + "</p>"; 
+			data = frontdata +result+ backdata;
+		}
+		println(data);
+		let write_data = data.as_bytes().to_owned();
+		return write_data;
+	} else {
+		return file_data;
+	}
+}
 
+fn gash_handle(command: ~str) -> ~str {
+	let mut cmd_line = command;
+        cmd_line = cmd_line.trim().to_owned();
+        let mut bg_flag = false;
+        if cmd_line.ends_with("&") {
+            cmd_line = cmd_line.trim_right_chars(&'&').to_owned();
+            bg_flag = true;
+        }  
+        handle_cmdline(cmd_line, bg_flag);
+	let temp_path = &path::Path("temp");
+	let output = io::read_whole_file_str(temp_path).unwrap();
+	os::remove_file(temp_path);
+	return output;
+}
+
+fn get_fd(fpath: &str, mode: &str) -> libc::c_int {
+    #[fixed_stack_segment]; #[inline(never)];
+
+    unsafe {
+        let fpathbuf = fpath.to_c_str().unwrap();
+        let modebuf = mode.to_c_str().unwrap();
+        return libc::fileno(libc::fopen(fpathbuf, modebuf));
+    }
+}
+
+fn exit(status: libc::c_int) {
+    #[fixed_stack_segment]; #[inline(never)];
+    unsafe { libc::exit(status); }
+}
+
+fn handle_cmd(cmd_line: &str, pipe_in: libc::c_int, pipe_out: libc::c_int, pipe_err: libc::c_int) {
+    let mut out_fd = pipe_out;
+    let mut in_fd = pipe_in;
+    let err_fd = pipe_err;
+    
+    let mut argv: ~[~str] =
+        cmd_line.split_iter(' ').filter_map(|x| if x != "" { Some(x.to_owned()) } else { None }).to_owned_vec();
+    let mut i = 0;
+    // found problem on redirection
+    // `ping google.com | grep 1 > ping.txt &` didn't work
+    // because grep won't flush the buffer until terminated (only) by SIGINT.
+    while (i < argv.len()) {
+        if (argv[i] == ~">") {
+            argv.remove(i);
+            out_fd = get_fd(argv.remove(i), "w");
+        } else if (argv[i] == ~"<") {
+            argv.remove(i);
+            in_fd = get_fd(argv.remove(i), "r");
+        }
+        i += 1;
+    }
+    
+    if argv.len() > 0 {
+        let program = argv.remove(0);
+        match program {
+            ~"help" => {println("This is a new shell implemented in Rust!")}
+            ~"cd" => {if argv.len()>0 {os::change_dir(&path::PosixPath(argv[0]));}}
+            //global variable?
+            //~"history" => {for i in range(0, history.len()) {println(fmt!("%5u %s", i+1, history[i]));}}
+            ~"exit" => {exit(0);}
+            _ => {
+			if out_fd != 1 {
+				let mut prog = run::Process::new(program, argv, run::ProcessOptions {
+                                                                                        env: None,
+                                                                                        dir: None,
+                                                                                        in_fd: Some(in_fd),
+                                                                                        out_fd: Some(out_fd),
+                                                                                        err_fd: Some(err_fd)
+                                                                                    });
+			prog.finish();
+		   	} else {
+				
+				let mut prog = run::Process::new(program, argv, run::ProcessOptions {
+                                                                                        env: None,
+                                                                                        dir: None,
+                                                                                        in_fd: Some(in_fd),
+                                                                                        out_fd: Some(get_fd("temp","w")),
+                                                                                        err_fd: Some(err_fd)
+                                                                                    });
+			prog.finish();
+			}
+
+                             // close the pipes after process terminates.
+                             if in_fd != 0 {os::close(in_fd);}
+                             if out_fd != 1 {os::close(out_fd);}
+                             if err_fd != 2 {os::close(err_fd);}
+                            }
+        }//match
+    }//if
+}
+
+fn handle_cmdline(cmd_line:&str, bg_flag:bool)
+{
+    // handle pipes
+    let progs: ~[~str] =
+        cmd_line.split_iter('|').filter_map(|x| if x != "" { Some(x.to_owned()) } else { None }).to_owned_vec();
+    
+    let mut pipes: ~[os::Pipe] = ~[];
+    
+    // create pipes
+    if (progs.len() > 1) {
+        for _ in range(0, progs.len()-1) {
+            pipes.push(os::pipe());
+        }
+    }
+        
+    if progs.len() == 1 {
+        if bg_flag == false { handle_cmd(progs[0], 0, 1, 2); }
+        else {task::spawn_sched(task::SingleThreaded, ||{handle_cmd(progs[0], 0, 1, 2)});}
+    } else {
+        for i in range(0, progs.len()) {
+            let prog = progs[i].to_owned();
+            
+            if i == 0 {
+                let pipe_i = pipes[i];
+                task::spawn_sched(task::SingleThreaded, ||{handle_cmd(prog, 0, pipe_i.out, 2)});
+            } else if i == progs.len() - 1 {
+                let pipe_i_1 = pipes[i-1];
+                if bg_flag == true {
+                    task::spawn_sched(task::SingleThreaded, ||{handle_cmd(prog, pipe_i_1.input, 1, 2)});
+                } else {
+                    handle_cmd(prog, pipe_i_1.input, 1, 2);
+                }
+            } else {
+                let pipe_i = pipes[i];
+                let pipe_i_1 = pipes[i-1];
+                task::spawn_sched(task::SingleThreaded, ||{handle_cmd(prog, pipe_i_1.input, pipe_i.out, 2)});
+            }
+        }
+    }
+}
 
